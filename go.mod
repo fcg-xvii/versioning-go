0e37aa5b3817a8647aae6247bd280dc7f0329ccd
jRFlint <flint@77i.su>
2020-08-21 20:35:54 +0300

    to v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/fcg-xvii/versioning-go
+module github.com/fcg-xvii/versioning-go/v2
 
 go 1.15
--- print/print.go
+++ print/print.go
@@ -2,5 +2,5 @@ package print
 
 // Print returns current version string
 func Print() string {
-	return "Current version is 1.0.2"
+	return "Current version is 2.0.0"
 }
